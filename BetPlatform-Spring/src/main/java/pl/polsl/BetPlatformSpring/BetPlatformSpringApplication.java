b0b593eb59e05a12fb2a5388462fdcbce71545ea
damirus534 <damianrussok@gmail.com>
2022-04-26 19:28:23 +0200

    Add generated project by Spring Initializer

---
+++ BetPlatform-Spring/src/main/java/pl/polsl/BetPlatformSpring/BetPlatformSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.BetPlatformSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetPlatformSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetPlatformSpringApplication.class, args);
+	}
+
+}
---
+++ BetPlatform-Spring/src/test/java/pl/polsl/BetPlatformSpring/BetPlatformSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.BetPlatformSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetPlatformSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
